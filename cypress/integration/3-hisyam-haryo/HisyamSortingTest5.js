da0c142eebf131aade9f8188b5132e4e4d6e7886
mashhisyam <haryoh48@gmail.com>
2021-10-29 12:32:44 +0700

    Mengganti Format

--- cypress/integration/3-hisyam-haryo/HisyamSortingTest5.js
+++ cypress/integration/3-hisyam-haryo/HisyamSortingTest5.js
@@ -6,9 +6,16 @@
 
 it.only('Saucedemo test', function(){
     cy.visit('https://www.saucedemo.com/')   
-    cy.get('#user-name.input_error.form_input').type('problem_user')
-    cy.get('#password.input_error.form_input').type('secret_sauce')
+    cy.get('#user-name.input_error.form_input')
+    .type('problem_user')
+
+    cy.get('#password.input_error.form_input')
+    .type('secret_sauce')
+
     cy.get('#login-button.submit-button.btn_action').click()
+
     cy.get('#add-to-cart-sauce-labs-backpack.btn.btn_primary.btn_small.btn_inventory').click()
+
     cy.get('#remove-sauce-labs-backpack.btn.btn_secondary.btn_small.btn_inventory').click()
+    
 })
