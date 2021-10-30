47fdd6ea8b7d9592e09aefb21caf2cd8eda04168
Laanaa <maulanamalik156357n@gmail.com>
2021-10-30 13:12:27 +0700

    Testcase 1 username dan password benar pada login

---
+++ cypress/integration/4-maulana-malik-ibrahim/MaulanaLoginTest3.js
@@ -0,0 +1,19 @@
+//Nama          : Maulana Malik Ibrahim
+//No Absen/NIM  : 12/1941720055
+//Kelas         : TI-3A
+//Test Case     : Login
+//Akun          : standard_User
+
+it.only('Saucedemo test', function(){
+    cy.visit('https://www.saucedemo.com/')
+       
+    cy.get('#user-name.input_error.form_input')
+    .type('standard_user')
+    // .type('standardt_user') //change to invalid username
+
+    cy.get('#password.input_error.form_input')
+    .type('secret_sauce')
+
+    cy.get('#login-button.submit-button.btn_action')
+    .click()
+})
