e37affa2b35944f18ff762f887bc898c5b75670c
Laanaa <maulanamalik156357n@gmail.com>
2021-10-30 13:35:38 +0700

    Testcase 4 username benar dan password benar uppercase pada login

--- cypress/integration/4-maulana-malik-ibrahim/MaulanaLoginTest4.js
+++ cypress/integration/4-maulana-malik-ibrahim/MaulanaLoginTest4.js
@@ -8,11 +8,10 @@ it.only('Saucedemo test', function(){
     cy.visit('https://www.saucedemo.com/')
        
     cy.get('#user-name.input_error.form_input')
-    .type('standard_user') 
+    .type('standard_user') //correct username
 
     cy.get('#password.input_error.form_input')
-    .type('secret_sauce')
-    // .type('SECRET_SAUCE') //Change to UPPERCASE password
+    .type('SECRET_SAUCE') //Change to UPPERCASE password
 
     cy.get('#login-button.submit-button.btn_action')
     .click()
