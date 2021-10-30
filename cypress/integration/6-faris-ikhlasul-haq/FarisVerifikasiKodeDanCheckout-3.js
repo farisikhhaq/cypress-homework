5741442b30129caaf25af30a7ee153cf5ab60597
xalll <farisikhlasulhaq@yahoo.co.id>
2021-10-30 14:21:12 +0700

    1941720062-FarisIkhlasulHaq-3

---
+++ cypress/integration/6-faris-ikhlasul-haq/FarisVerifikasiKodeDanCheckout-3.js
@@ -0,0 +1,28 @@
+//Nama          : Faris Ikhlasul Haq
+//No Absen/NIM  : 07/1941720062
+//Kelas         : TI-3A
+//Test Case     : Verify Checkout(Valid Firstname & Lastname , Valid Zip)
+//Akun          : standart_user
+
+/// <reference types="cypress"/>
+
+describe('Verify Checkout Valid Fistname & Username, Valid Zip', () => {
+    it('visit web saucedemo', () => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('#user-name.input_error.form_input')
+            .type('problem_user')
+        cy.get('#password.input_error.form_input')
+            .type('secret_sauce')
+        cy.contains('Login').click()
+        cy.get('#add-to-cart-sauce-labs-backpack.btn.btn_primary.btn_small.btn_inventory').click()
+        cy.get('.shopping_cart_link').click()
+        cy.contains('Checkout').click()
+        cy.get('#first-name.input_error.form_input')
+            .type('Faris')
+        cy.get('#last-name.input_error.form_input')
+            .type('Ikhlasul H')
+        cy.get('#postal-code.input_error.form_input')
+            .type('65152')
+        cy.contains('Continue').click()
+    })
+})
